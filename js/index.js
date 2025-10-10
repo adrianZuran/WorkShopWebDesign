c8a1d5e054ec240d4fd4943df346834889be1966
Adrian_students <112798667+adrianZuran@users.noreply.github.com>
2025-10-11 01:39:07 +0700

    uts

---
+++ js/index.js
@@ -0,0 +1,25 @@
+let i, b, result;
+i = 1
+b = 2
+
+function angka (i, b){
+    let hasil;
+    hasil = i + b;
+    return hasil;
+}
+
+let namaObjek  = {
+    nama: "kucur",
+}
+
+let namaArray = ["Jossjiss"]
+
+result = i + b;
+// biasa
+console.log(result)
+// Function
+console.log(angka(5,6))
+// Object
+console.log(namaObjek.nama);
+// Array
+console.log(namaArray[0])
